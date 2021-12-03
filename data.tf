5463340c793b9449e76124f8b71530fa343d07ec
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-12-03 10:09:11 +1000

    fixed

--- data.tf
+++ data.tf
@@ -1,12 +1,22 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
+data "aws_ami" "amazon_linux_free_tier" {
+  most_recent = true
+  owners = [
+    "amazon"
+  ]
+
+  filter {
+    name = "name"
+    values = [
+      "amzn2-ami-hvm-*-x86_64-gp2"
+    ]
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
+data "aws_vpc" "default_vpc" {
+  filter {
+    name = "is-default"
+    values = [
+      true
+    ]
+  }
 }
--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "private_key" {
   sensitive = true
   value     = tls_private_key.ssh_key.private_key_pem
 }
+
+output "public_ip" {
+  value = aws_instance.virtual_key.public_ip
+}
